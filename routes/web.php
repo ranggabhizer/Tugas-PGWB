9655a875867196813762ae1cbe7a60e26e374022
ranggabhizer <bintang01rangga@gmail.com>
2021-10-19 10:10:51 +0700

    Tugas 2: Base admin template

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('adminapp.master');
+});
+
+Route::get('/dashboard', function () {
+    return view('admindashboard');
 });
